b2cac5bf40affd66d17d089ab8dca10713cbf662
inukamikorone <3034221968@qq.com>
2020-12-15 11:28:20 +0800

    first commit

---
+++ 01-文件版四则运算式.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+int main(void){
+	
+	system("pause");
+	return EXIT_SUCCESS;
+}
+
---
+++ code/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+scoreTable = np.mat([[5,1,4],
+                    []])
